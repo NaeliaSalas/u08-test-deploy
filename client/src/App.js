f4a8043a033962338b0dfd0852a48c36b7e62414
Naelia Salas <naeliasalas@gmail.com>
2022-05-10 11:33:06 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/blog">Blog</Link> | { "" }
+        <Link to="/about">About</Link>| { "" }
+        <Link to="/home">Home</Link>
+      </nav>
+            <Outlet />
+
+    </div>
+  );
+}
+
+export default App;
